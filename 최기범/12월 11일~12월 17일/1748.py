27ec15ed5dbf9c3383883e01b2e5cae4cddd0a4b
kibeom <kibeom2000@naver.com>
2023-12-17 12:56:23 +0900

    commit_1748_추가 설명

--- 최기범/12월 11일~12월 17일/1748.py
+++ 최기범/12월 11일~12월 17일/1748.py
@@ -1,3 +1,8 @@
+'''
+1번째 시도 : Dynamic Programming 기법으로 풀려고 했으나 메모리 초과
+2번째 시도 : 반복문 1번 통과(구현) 으로 풀려고 했으나 메모리 초과
+3번째 시도 : 규칙 찾기 (10**(숫자의 자릿수) - 1) * (숫자의 자릿수)
+'''
 # n = int(input())
 # d = [0] * (n+1)
 # if n < 10 :
@@ -15,7 +20,7 @@
 # for i in range(1,n+1) :
 #     result += len(str(i))
 # print(result)
-    
+
 n = input()
 result = 0
 for i in range(1,len(n)) :
